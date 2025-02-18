ce5f0b4b7c73aa7d4b44fc6622c88bf731049aae
Josh De Ment <joshdement12@gmail.com>
2025-02-17 18:58:06 -0600

    Insert email function created

--- query_db.py
+++ query_db.py
@@ -1,19 +1,25 @@
 import mysql.connector
-
-try:
-    conn = mysql.connector.connect(
-        host = 'phishingemailproject.cn0geogwil7k.us-east-2.rds.amazonaws.com',
-        user = 'admin',
-        password = 'EmailProject1',
-        database = 'email_database',
-    )
-    
-    cursor = conn.cursor()
-
-except mysql.connector.Error as err:
-    print(f"Error: {err}")
+import datetime
+  
+def insert_email(id, subject, score, issues):
+    try:
+        conn = mysql.connector.connect(
+            host = 'phishingemailproject.cn0geogwil7k.us-east-2.rds.amazonaws.com',
+            user = 'admin',
+            password = 'EmailProject1',
+            database = 'email_database',
+        )
         
-finally:
-    if conn:
-        cursor.close()
-        conn.close()
+        cursor = conn.cursor()
+        
+        now = datetime.timezone.utc()
+        cursor.execute("INSERT INTO Email (email_id, user_id, subject, timestamp, score, has_attachment, issues) VALUES (%d, 1, %s, %s, %d, 0, %s)", (id, subject, now, score, issues))
+        
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
+    
+    
+    finally:
+        if conn:
+            cursor.close()
+            conn.close()
